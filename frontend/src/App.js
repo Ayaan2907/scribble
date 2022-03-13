7608d1503c104d0edabb0136145098e5a5cf101a
Ayaan Kaifullah <ayaanansari7921@outlook.com>
2022-03-14 01:49:35 +0530

    socket.io 1. socket connection established between client and server 2. now implimenting    1. rooms    2. message interchanging UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { io } from "socket.io-client";
+import "./App.css";
+const socket = io("http://localhost:4000");
 
 function App() {
+  const [socketId, setSocketId] = useState("");
+  socket.on("connect", () => {
+    setSocketId(socket.id);
+    console.log(`Connected to server with id ${socket.id}`);
+  });
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>current client socket ID :    {socketId}</div>
       </header>
     </div>
   );
---
+++ frontend/src/components/index.js
@@ -0,0 +1,2 @@
+// export { default as InputBox } from './Chatbox/InputBox'
+// export {default as MessageBox} from './Chatbox/MessageBox'
