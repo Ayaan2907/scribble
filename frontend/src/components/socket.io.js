a8ebfc415605d4e5fad019dbe79fee36ad86f978
Ayaan Kaifullah <ayaanansari7921@outlook.com>
2022-05-01 04:12:22 +0530

    commiting changes to change device

---
+++ frontend/src/components/socket.io.js
@@ -0,0 +1,4 @@
+// a working component responsible for habdeling the socket connection 
+// as soon as it mounts a socket connection is established
+// the connection will be exposed to children using contextApis
+ 
